65accf2d495a1d437b583e814f08169f2c8efa20
Joe Moon <joe@xoxomoon.com>
2023-05-05 17:28:00 -0700

    devicon colors

--- lua/sodium/init.lua
+++ lua/sodium/init.lua
@@ -2,5 +2,6 @@ local lush = require("lush")
 
 local ui = require("sodium.ui")
 local syntax = require("sodium.syntax")
+local dev_icons = require("sodium.dev-icons")
 
-return lush.merge({ syntax, ui })
+return lush.merge({ syntax, ui, dev_icons })
