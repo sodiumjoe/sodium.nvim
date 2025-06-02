db73bd52f6772dcc28b3ea698a7740a70ee1abf6
Joe Moon <joe@xoxomoon.com>
2025-06-01 21:13:31 -0700

    fix build script

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -1,10 +1,12 @@
 local lushwright = require("shipwright.transform.lush")
+package.path = "lua/?.lua;" .. package.path
+
 run(
-	require("sodium"),
-	-- generate lua code
-	lushwright.to_lua,
-	-- write the lua code into our destination.
-	-- you must specify open and close markers yourself to account
-	-- for differing comment styles, patchwrite isn't limited to lua files.
-	{ patchwrite, "colors/sodium-dark.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
+  require("sodium/init"),
+  -- generate lua code
+  lushwright.to_lua,
+  -- write the lua code into our destination.
+  -- you must specify open and close markers yourself to account
+  -- for differing comment styles, patchwrite isn't limited to lua files.
+  { patchwrite, "colors/sodium-dark.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
 )
